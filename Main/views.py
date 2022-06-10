fb73330136c4cf688ad857ac49a9ea3463a08f44
latototita <pearlmartbusinesses@gmail.com>
2022-06-10 11:43:12 -0500

    Contact form

--- Main/views.py
+++ Main/views.py
@@ -2,11 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context={}
-    return render(request,'index.html',context)
-
-
-def  contact(request):
     subject = "Contact Laban"
     message = request.POST.get('message')
     name = request.POST.get('name')
@@ -27,7 +22,8 @@ def  contact(request):
                 fail_silently = True,
                 )
         messages.success(request, f'We have recived your message, You will recive and email confirming it soon, Have a lovely day')
-        return redirect('index')
+        return render(request,'index.html',context)
     else:
-        messages.success(request, f'Dear {name},\n Sorry But there may be an error in the System, \n Please Contact me on pearljob@gmail.com')
-        return redirect('index')
+        if name != None:
+            messages.success(request, f'Dear {name},\n Sorry But there may be an error in the System, \n Please Contact me on pearljob@gmail.com')
+        return render(request,'index.html',context)
